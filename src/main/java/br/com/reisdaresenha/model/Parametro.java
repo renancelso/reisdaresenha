3a3bf6759bb394b52cdf3ba764d28f3619faf8d7
rcelso <rcelso@ford.com>
2020-07-21 23:38:52 -0300

    Ajustando tela inicial pra ficaer mais simples e sem datatable nos participantes

---
+++ src/main/java/br/com/reisdaresenha/model/Parametro.java
@@ -0,0 +1,67 @@
+package br.com.reisdaresenha.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ * @author Renan Celso
+ */
+@Entity
+@Table(name="parametro")
+public class Parametro implements Serializable {
+	
+	private static final long serialVersionUID = 289669369420056911L;
+
+	@Id	
+	@Column(name = "chave", nullable = false, length=255)
+	private String chave;	
+	
+	@Column(name = "valor", nullable = false, length=255)
+	private String valor;
+
+	public String getChave() {
+		return chave;
+	}
+
+	public void setChave(String chave) {
+		this.chave = chave;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((chave == null) ? 0 : chave.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Parametro other = (Parametro) obj;
+		if (chave == null) {
+			if (other.chave != null)
+				return false;
+		} else if (!chave.equals(other.chave))
+			return false;
+		return true;
+	}	
+}
--- src/main/java/br/com/reisdaresenha/rest/ExemploClientRestGet.java
+++ src/main/java/br/com/reisdaresenha/rest/ExemploClientRestGet.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+import com.google.gson.Gson;
 
 //https://api.cartolafc.globo.com/mercado/status
 //https://api.cartolafc.globo.com/atletas/mercado
@@ -19,14 +22,20 @@ public class ExemploClientRestGet {
 		
 		try {						
 			
-			String endPoint = "https://api.cartolafc.globo.com/ligas?q=RDR%202020";
+			String endPoint = "https://login.globo.com/api/authentication";
 			
 			HttpClient client = new HttpClient();
 
-			GetMethod method = new GetMethod(endPoint);
+			PostMethod method = new PostMethod(endPoint);
 			method.setRequestHeader("Connection", "keep-alive");
 			method.setRequestHeader("Accept", "*/*");
-			method.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			method.setRequestHeader("Content-type", "application/x-www-form-urlencoded");							  
+			
+			NameValuePair nvp1 = new NameValuePair("payload", "{\"payload\": {\"email\":\"renancelso@globo.com\",\"password\":\"04162003\",\"serviceId\":4728}}");						
+			
+			method.setRequestBody(new NameValuePair[]{nvp1});
+			
+			//{\"payload\": {\"email\":\"renancelso@globo.com\",\"password\":\"04162003\",\"serviceId\":4728}}
 				
 			try {
 			
@@ -45,9 +54,7 @@ public class ExemploClientRestGet {
 				JSONArray jsonArray = (JSONArray) parser.parse(jsonResponse);		
 				
 				JSONObject jsonObject = (JSONObject) jsonArray.get(0);		
-				
-				String imagem = (String) jsonObject.get("imagem");
-															
+											
 				method.releaseConnection();
 
 			} catch (IOException e) {
